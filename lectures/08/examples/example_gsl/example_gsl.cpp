98383908f891ccf5b79ec3251224272745c58eed
Pasquale Claudio Africa <pafrica@sissa.it>
2025-11-12 09:59:40 +0100

    clang-format

--- lectures/08/examples/example_gsl/example_gsl.cpp
+++ lectures/08/examples/example_gsl/example_gsl.cpp
@@ -3,27 +3,21 @@
 #include <cmath>
 #include <iostream>
 
-double
-my_function(double x, void *params)
-{
-  return sin(x);
-}
+double my_function(double x, void *params) { return sin(x); }
 
-int
-main()
-{
+int main() {
   gsl_integration_workspace *workspace = gsl_integration_workspace_alloc(1000);
 
-  double       result, error;
+  double result, error;
   gsl_function F;
   F.function = &my_function;
-  F.params   = nullptr;
+  F.params = nullptr;
 
   const double lower_limit = 0.0;
   const double upper_limit = M_PI;
 
-  gsl_integration_qags(
-    &F, lower_limit, upper_limit, 1e-8, 1e-8, 1000, workspace, &result, &error);
+  gsl_integration_qags(&F, lower_limit, upper_limit, 1e-8, 1e-8, 1000,
+                       workspace, &result, &error);
 
   std::cout << "Result: " << result << "\n";
   std::cout << "Estimated error: " << error << "\n";
